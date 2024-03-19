fe70b4e7a5649c27141dfc7a7350813b4b5594d7
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-03-19 23:32:22 +0530

    Functions

---
+++ 05-Functions/function.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello World!");
+
+    another_function();
+}
+
+fn another_function() {
+    println!("Hello World from another function!");
+}
---
+++ 05-Functions/operational_function.rs
@@ -0,0 +1,8 @@
+fn main() {
+    sum(4,6);
+}
+
+fn sum(x : i32, y : i32) {
+    let z : i32 = x + y;
+    println!("Sum of x & y is {}", z);
+}
