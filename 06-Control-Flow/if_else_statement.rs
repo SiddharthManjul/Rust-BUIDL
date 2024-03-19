c4daf676694c41f7e3ee973f0ce076dd50836f18
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-03-19 23:53:01 +0530

    If Else Statement

---
+++ 06-Control-Flow/if_else_statement.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let number : u16 = 3;
+
+    if number < 5 {
+        println!("Condition is true, the number is less than 5!");
+    } else {
+        println!("Condition is false, the number is greater than 5!");
+    }
+}
