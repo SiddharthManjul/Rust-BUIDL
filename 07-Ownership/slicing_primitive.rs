eb690c3ecafeacfb25289a50cf8a27e71cca2315
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-03-22 23:50:11 +0530

    Slice Type

---
+++ 07-Ownership/slicing_primitive.rs
@@ -0,0 +1,23 @@
+// Slicing without Slice Type:
+
+fn main() {
+    let mut s = String::from("Hello World");
+
+    let word = first_word(&s);
+    println!("{}", word);
+
+    s.clear();
+
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
