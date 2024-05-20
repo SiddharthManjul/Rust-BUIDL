5742395e056d6cd8d3e97b1fa9ff1761e68c9d85
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-20 23:53:33 +0530

    Documentation

--- playground/src/funcs.rs
+++ playground/src/funcs.rs
@@ -1,15 +1,32 @@
+/// Functions: add and subtract
+/// 
+/// # Argument: (num: u32)
+/// 
+/// # Example:
+/// ```
+/// let x = 5;
+/// let y = add(x);
+/// ```
+
 pub fn add(num: u32) -> u32 {
     num + 5
 }
+
+/// ```
+/// let z = subtract(x);
+/// ```
 
 pub fn subtract(num: u32) -> u32 {
     num - 5
 }
 
-// To run a test we have to use "cargo test" command.
-// To let the tests print out the values, use "cargo test -- --nocapture" command.
-// To run a single test put the test function name after cargo test: "cargo test adds_a_number_test"
-// In case a test fails, use "RUST_BACKTRACE=1 cargo test test_name" to check the issues.
+/// pub keyword is used to make a function public.
+/// Use "cargo doc" to create documentation from the comment in html format and to open it run command "cargo doc --open".
+
+/// To run a test we have to use "cargo test" command.
+/// To let the tests print out the values, use "cargo test -- --nocapture" command.
+/// To run a single test put the test function name after cargo test: "cargo test adds_a_number_test"
+/// In case a test fails, use "RUST_BACKTRACE=1 cargo test test_name" to check the issues.
 
 #[cfg(test)]
 mod test {
@@ -31,5 +48,3 @@ mod test {
         assert_eq!(y, 95);
     }
 }
-
-// pub keyword is used to make a function public.
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,9 +1,13 @@
 mod funcs;
-mod functions; // Letting the file know that there is a file called funcs.rs existing.
-
+mod functions;
+/// Letting the file know that there is a file called funcs.rs existing.
 use crate::funcs::{add, subtract};
-use crate::functions::other_funcs::divide; // Importing add & subtract functions from funcs. We can also use * for the same.
+use crate::functions::other_funcs::divide;
+/// Importing add & subtract functions from funcs. We can also use * for the same.
+///  Everything in Rust is immutable by default.
+
 fn main() {
+    //To make a variable mutable we need to use mut keyword like this: let mut x: u32 = 50;
     let mut x: u32 = 50;
     println!("x is {}", x);
 
@@ -19,6 +23,3 @@ fn main() {
     let p = divide(x);
     println!("p is {}", p);
 }
-
-// Everything in Rust is immutable by default.
-// To make a variable mutable we need to use mut keyword like this: let mut x: u32 = 50;
