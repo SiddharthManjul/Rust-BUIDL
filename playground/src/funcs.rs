f91441553046000d9c69d9688c47ddb7aaeda0de
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-20 21:13:19 +0530

    First Unit Tests

--- playground/src/funcs.rs
+++ playground/src/funcs.rs
@@ -6,4 +6,26 @@ pub fn subtract(num: u32) -> u32 {
     num - 5
 }
 
+// To run a test we have to use cargo test command
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn adds_a_number_test() {
+        let x = 100;
+        let y = add(x);
+        println!("x and y are from test: {} {}", x, y);
+        assert_eq!(y, 105);
+    }
+
+    #[test]
+    fn subtract_a_number_test() {
+        let x = 100;
+        let y = subtract(x);
+        println!("x and y are from test: {} {}", x, y);
+        assert_eq!(y, 95);
+    }
+}
+
 // pub keyword is used to make a function public.
--- playground/src/functions/other_funcs.rs
+++ playground/src/functions/other_funcs.rs
@@ -5,3 +5,24 @@ pub fn multiply(num: u32) -> u32 {
 pub fn divide(num: u32) -> u32 {
     num / 5
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn multiplies_a_number_test() {
+        let x = 4;
+        let y = multiply(x);
+        println!("x and y are from test: {} {}", x, y);
+        assert_eq!(y, 20);
+    }
+
+    #[test]
+    fn divides_a_number_test() {
+        let x = 50;
+        let y = divide(x);
+        println!("x and y are from test: {} {}", x, y);
+        assert_eq!(y, 10);
+    }
+}
