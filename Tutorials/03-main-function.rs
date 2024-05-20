008c89161bedea2cb1218b9e57a361d3a1f3181a
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-20 15:40:14 +0530

    Tutorial 03 Files

---
+++ Tutorials/03-main-function.rs
@@ -0,0 +1,21 @@
+mod funcs;
+mod functions; // Letting the file know that there is a file called funcs.rs existing.
+
+use crate::funcs::{add, subtract};
+use crate::functions::other_funcs::divide;// Importing add & subtract functions from funcs. We can also use * for the same.
+fn main() {
+    let mut x: u32 = 50;
+    println!("x is {}", x);
+
+    let y = add(x);
+    println!("y is {}", y);
+
+    x = 60;
+    println!("x is {}", x);
+
+    let z = subtract(x);
+    println!("z is {}", z);
+
+    let p = divide(x);
+    println!("p is {}", p);
+}
