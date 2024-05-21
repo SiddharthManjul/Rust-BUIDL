626bb3d451d4f996348cf2229ab3a561006d6fa6
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-22 01:33:17 +0530

    Dynamic Variables

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,52 +1,31 @@
-const TEAM: &str = "KKR";
-
 fn main() {
 
-    println!("{} is the first team qualifying for Finals in IPL 2024", TEAM);
+    // String Literal
+    let name: &str = "Siddharth";
+    println!("Name is {:?}", name);
 
-    // Stack
-    let x: i32;
-    x = 2;
-    println!("x is {}", x);
+    // Dynammic String
+    let full_name: String = String::from("Siddharth Manjul");
+    // Another way: let full_name: String = "Siddharth Manjul".to_string();
+    println!("Full name is {:?}", full_name);
+    println!("Full name stored in memory {:p}", &full_name);
 
-    let y: i32 = 4;
-    println!("y is {}", y);
+    // String Slice
+    let name_slice: &str = &full_name[10..16];
+    println!("Sliced Name is {:?}", name_slice);
 
-    // For loop
-    for i in 0..=y {
-        if i != 4 {
-            print!("{}, ", i);
-        } else {
-            println!("{}", i);
-        }
-    }
+    // Vector
+    let mut chars: Vec<char> = Vec::new();
+    chars.insert(0, 'h');
+    chars.insert(1, 'e');
+    chars.insert(2, 'l');
+    chars.push('l');
+    chars.push('o');
+    chars.push('.');
+    dbg!(&chars);
 
-    // Mutable Variable
-    let mut z: i32 = 5;
-    print!("z was {}.", z);
-    z = 10;
-    println!(" After mutating, z is {}!", z);
+    let removed_char: char = chars.pop().unwrap();
+    println!("Removed character is {}", removed_char);
 
-    // Float value
-    let freezing_temp: f64 = -2.4;
-    println!("Freezing Temperature is {}", freezing_temp);
-    
-    // Boolean Value
-    let is_zero_remainder: bool = 10 % 2 == 0;
-    println!("Is Zero remainder? {}", is_zero_remainder);
-
-    // Character
-    // Whenever working with Characters, always use single quotes or backticks, that's how rust know that it's a Character.
-    let first_character_of_name: char = 'S';
-    println!("The first Character of my Name is {}", first_character_of_name);
-
-    let emoji_char: char = '😎';
-    println!("Emoji Character is {}", emoji_char);
-
-    // array of floats'
-    let my_floats: [f32; 10] = [0.11; 10];
-    println!("The float array is {:?}", my_floats);
-
-    let my_floats_new: [f32; 10] = my_floats.map(|n| n + 2.0);
-    println!("The new array is {:?}", my_floats_new);
+    println!("Now the array is {:?}", chars);
 }
