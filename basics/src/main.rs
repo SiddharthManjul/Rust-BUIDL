c153590cb8a196043a7609102461083cfcc8711f
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-22 11:10:07 +0530

    Closures, Number Literal and Raw - String Literal

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,49 +1,19 @@
 fn main() {
+    
+    // Closures
+    let num: i32 = 5;
+    let add_num = |x: i32| x + num;
+    let new_num = add_num(7);
+    dbg!(new_num);
 
-    // String Literal
-    let name: &str = "Siddharth";
-    println!("Name is {:?}", name);
+    // Number Literals
+    println!("Big number is {}", 98_222_000);
+    println!("Hex is {}", 0xff);
+    println!("Octal is {}", 0o77);
+    println!("Binary is {}", 0b1111_0000);
+    println!("Bytes 'A' is {}", b'A');
 
-    // Dynammic String
-    let full_name: String = String::from("Siddharth Manjul");
-    // Another way: let full_name: String = "Siddharth Manjul".to_string();
-    println!("Full name is {:?}", full_name);
-    println!("Full name stored in memory {:p}", &full_name);
-
-    // String Slice
-    let name_slice: &str = &full_name[10..16];
-    println!("Sliced Name is {:?}", name_slice);
-
-    // Vector
-    let mut chars: Vec<char> = Vec::new();
-    chars.insert(0, 'h');
-    chars.insert(1, 'e');
-    chars.insert(2, 'l');
-    chars.push('l');
-    chars.push('o');
-    chars.push('.');
-    dbg!(&chars);
-
-    let removed_char: char = chars.pop().unwrap();
-    println!("Removed character is {}", removed_char);
-
-    println!("Now the array is {:?}", chars);
-
-    // Creating a collection of letters
-    chars.iter().for_each(|c| print!("{}", c));
-    print!("\n");
-
-    // Another way
-    let new_chars: Vec<char> = vec!['h','e','l','l','o'];
-    dbg!(&new_chars);
-
-    let collected: String = new_chars.iter().collect();
-    println!("Collected String is {}", collected);
-
-    for c in new_chars {
-        print!("{}", c);
-        if c == 'o' {
-            println!(", world!");
-        }
-    }
+    // Raw - String Literal
+    let text: &str = r#"{"Message" : "Rust is Awesome"}"#;
+    dbg!(text);
 }
