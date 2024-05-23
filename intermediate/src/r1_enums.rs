3baf4dad4757a143d64f51b8a27620c601c65410
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-23 20:52:44 +0530

    Option Enum with Some or None

--- intermediate/src/r1_enums.rs
+++ intermediate/src/r1_enums.rs
@@ -1,3 +1,5 @@
+use core::num;
+#[allow(dead_code)]
 #[derive(Debug)]
 
 enum CarColour {
@@ -14,9 +16,18 @@ fn create_car_colour() -> CarColour {
 
 // <T, E> - Generic Type or Generics
 #[derive(Debug)]
+#[allow(dead_code)]
 enum GivenResult<T, E> { 
     Ok(T),
     Err(E)
+}
+
+// Some or None
+#[derive(Debug)]
+#[allow(dead_code)]
+enum GivenOption<T> { 
+    None,
+    Some(T)
 }
 
 fn check_under_five(num_check: u8) -> GivenResult<u8, String> {
@@ -24,6 +35,15 @@ fn check_under_five(num_check: u8) -> GivenResult<u8, String> {
         GivenResult::Ok(num_check)
     } else {
         GivenResult::Err("Not Under 5!".to_string())
+    }
+}
+
+fn remainder_zero(num_check: f32) -> GivenOption<f32> {
+    let remainder = num_check % 10.0;
+    if remainder != 0.0 {
+        GivenOption::Some(remainder)
+    } else {
+        GivenOption::None
     }
 }
 
@@ -41,5 +61,8 @@ mod test {
 
         let is_number_under_five = check_under_five(7);
         dbg!(is_number_under_five);
+
+        let remainder = remainder_zero(12.2);
+        dbg!(remainder);
     }
 }
