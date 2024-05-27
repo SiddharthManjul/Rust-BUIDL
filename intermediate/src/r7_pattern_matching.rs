adae1fefe66343c684ccc219bef43b1dd4af20e1
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-27 21:01:35 +0530

    Pattern Matching - Options

--- intermediate/src/r7_pattern_matching.rs
+++ intermediate/src/r7_pattern_matching.rs
@@ -3,6 +3,7 @@ mod test {
     #[allow(dead_code, unused_variables, unused_imports)]
     use super::*;
 
+    // Integers
     #[test]
     fn test_match_literals() {
         let number: i32 = 20;
@@ -13,6 +14,30 @@ mod test {
             _ => "It was something else!",
         };
 
+        println!("{}", res);
+    }
+
+    // Options
+    #[test]
+    fn test_match_options() {
+        let some_num: Option<i32> = Some(10);
+        // let prob_none: Option<i32> = None; --- Using match statement on this one will go for None case only.
+
+        let res = match some_num {
+            Some(i) => i,
+            None => {
+                panic!("There was some Problem!");
+            }
+        };
+
+        // Another way
+        if let Some(i) = some_num {
+            println!("From if statement, {}", i);
+        } else {
+            panic!("There was a Problem!");
+        }
+
+        println!("{:?}", some_num);
         println!("{}", res);
     }
 }
