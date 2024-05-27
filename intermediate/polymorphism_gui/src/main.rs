f46ac1956fd68559ba36143b4ef9cc96a003ecf6
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-27 17:02:34 +0530

    Polymorphism

--- intermediate/polymorphism_gui/src/main.rs
+++ intermediate/polymorphism_gui/src/main.rs
@@ -1,3 +1,38 @@
+use polymorphism_gui::*;
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // Do something
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 100,
+                height: 100,
+                options: vec![
+                    String::from("yes"),
+                    String::from("no"),
+                    String::from("maybe"),
+                ]
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 50,
+                label: String::from("click me")
+            })
+        ]
+    };
+
+    println!("{:?}", screen.components);
+
+    screen.run();
 }
