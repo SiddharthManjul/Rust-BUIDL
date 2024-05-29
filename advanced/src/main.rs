0683ab2ef2937dc77d58a800b4c3a7d941f9454d
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-30 03:20:44 +0530

    Procedural Macro Testing

--- advanced/src/main.rs
+++ advanced/src/main.rs
@@ -1,4 +1,5 @@
 mod r1_declarative_macro;
+mod r2_proc_macros;
 
 fn main() {
     println!("Hello, world!");
---
+++ advanced/src/r2_proc_macros.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+
+mod tests {
+
+    use ai_func_proc_macro::function_to_string;
+
+    const OUTPUT: &str = "";
+
+    #[function_to_string]
+    fn some_function_for_ai_llm(_whatever_param: &str) {
+        /// This is an awesome function
+        /// We shall give it to an AI to guess and output
+        /// In a structured manner
+        println!("{}", OUTPUT);
+    }
+
+    #[test]
+    fn tests_proc_macro() {
+        
+        let x: &str = some_function_for_ai_llm("Some Large Prompt");
+        dbg!(x);
+    }
+}
