8f01f53f024ad2aa31014057aac7f9a35dbfdbd2
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-05-30 02:40:46 +0530

    AI Function Macro

--- advanced/ai_func_proc_macro/src/lib.rs
+++ advanced/ai_func_proc_macro/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate proc_macro;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, ItemFn};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_attribute]
+pub fn function_to_string(_attr: TokenStream, item: TokenStream) -> TokenStream {
+
+    // Parse the Input Function
+    let input_fn: ItemFn = parse_macro_input!(item as ItemFn);
+
+    // Create String Representation of Function
+    let function_str: String = format!("{}", input_fn.to_token_stream());
+
+    // Define a new Function with the same Signature as Input Function
+    let fn_ident: proc_macro2::Ident = input_fn.sig.ident;
+    let fn_inputs: syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma> = input_fn.sig.inputs;
+    let fn_generics: syn::Generics = input_fn.sig.generics;
+
+    // Generate Output Function
+    let output: proc_macro2::TokenStream = quote! {
+        pub fn #fn_ident #fn_generics(#fn_inputs) -> &'static str {
+            #function_str
+        }
+    };
+
+    output.into()
+
 }
